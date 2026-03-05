e88e77613313b868ab2d8c1a9f13bc0ebd75f8b4
Jayu-29 <jaibaradia29@gmail.com>
2026-03-05 12:16:28 +0000

    Challenge-09 Completed

--- src/09-password-strength.js
+++ src/09-password-strength.js
@@ -27,4 +27,34 @@
  */
 export function checkPasswordStrength(password) {
   // Your code here
+  let count = 0;
+  if(typeof password !== "string" || password.length === 0){
+    return "weak";
+  }
+  if(password.length >= 8){
+    count++;
+  }
+  if(/[a-z]/.test(password)){
+    count++;
+  }
+  if(/[A-Z]/.test(password)){
+    count++;
+  }
+  if(/[0-9]/.test(password)){
+    count++;
+  }
+  if(/[!@#$%^&*()_+\-=\[\]{}|;:,.<>?]/.test(password)){
+    count++;
+  }
+  if(count<= 1){
+    return "weak";
+  }
+  else if(count<= 3){
+    return "medium";
+  }
+  else if(count == 4){
+    return "strong";
+  }
+  else 
+    return "very strong";
 }
