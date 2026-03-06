f088d9b954d5dcfce69b008a05f0d641152d8806
Jayu-29 <jaibaradia29@gmail.com>
2026-03-06 20:37:07 +0000

    Challenge-12 Completed

--- src/12-season-activity.js
+++ src/12-season-activity.js
@@ -30,6 +30,57 @@
  * @param {number} temperature - Current temperature in Celsius
  * @returns {{ season: string, activity: string } | null}
  */
-export function getSeasonActivity(month, temperature) {
+export function getSeasonActivity(month, temp) {
   // Your code here
+  let season;
+  let activity;
+  switch (month) {
+    case 1:
+    case 2:
+    case 12:
+      season = "Winter";
+      if(temp < 0){
+        activity = "skiing";
+      }
+      else if(temp >= 0){
+        activity = "ice skating";
+      }
+      break;
+    case 3:
+    case 4:
+    case 5:
+      season = "Spring";
+      if(temp > 20){
+        activity = "hiking";
+      }
+      else if(temp <= 20){
+        activity = "museum visit";
+      }
+      break;
+    case 6:
+    case 7:
+    case 8:
+      season = "Summer";
+      if(temp > 35){
+        activity = "swimming";
+      }
+      else if(temp <= 35){
+        activity = "cycling";
+      }
+      break;
+    case 9:
+    case 10:
+    case 11:
+      season = "Autumn";
+      if(temp > 15){
+        activity = "nature walk";
+      }
+      else if(temp <= 15){
+        activity = "reading at a cafe";
+      }
+      break;
+    default:
+      return null;
+  }
+  return{season,activity};
 }
